effdd19beaacbdebc30a8caa5c5b9c5c9393830e
Sean Cafe <seanamir30@gmail.com>
2023-04-02 00:53:14 +0800

    Removed non-existing favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,6 @@ export default function Home() {
         <title>Sean</title>
         <meta name="description" content="Hi I'm Sean, I'm a software engineer specialized in typescript" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossOrigin="anonymous"/>
       </Head>
       <main>
